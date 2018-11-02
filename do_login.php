a24e22d344591d43304d06d94453b54a3e794671
ab238 <ab238@hdm-stuttgart.de>
2018-11-02 08:28:15 +0100

    hallo

---
+++ do_login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+include_once "userdata.php";
+
+if (isset($_POST["login"])) {
+
+    $name = $_POST["name"];
+    $passwort = $_POST["passwort"];
+
+
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+    $pdo=new PDO($dsn, $dbuser, $dbpass, $options);
+
+
+    $statement = $pdo->prepare("SELECT * FROM Anmeldung WHERE name=:name AND passwort=:passwort");
+    $statement->execute(array(':name' => $name, ':passwort' => $passwort));
+    $user = $statement->fetch();
+
+    $_SESSION["angemeldet"]=$user["id"];
+
+    if ($user !== false) {
+        header("location: startseite.php");
+    } else {
+        echo "E-Mail oder Passwort war ungültig";
+    }
+}

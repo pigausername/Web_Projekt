cc4eadf1c8856c090b4d89e6d53ec9212edf3fa2
Minh_vh <40471018+mv065@users.noreply.github.com>
2018-11-06 19:23:01 +0100

    Register Update
    Register.php funktioniert endlich

--- register.php
+++ register.php
@@ -2,13 +2,11 @@
 
 session_start();
 
-$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de;dbname=u-rk067',
-    'rk067',
-    'joB9Wahx5J',array('charset'=>'utf8'));
+include_once "userdata.php";
 
-$statement = $pdo->prepare("INSERT INTO `*bla*` (`username`, `password`, `email`, `fist_name`,`last_name`) VALUES ('$username', '$password', '$firstname', '$lastname')");
+$pdo=new PDO($dsn, $dbuser, $dbpass);
 
-if (isset($_POST['register_btn'])) {
+if (isset($_POST['register'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $repeatpassword = $_POST['repeatpassword'];
@@ -18,12 +16,45 @@ if (isset($_POST['register_btn'])) {
 
     if ($_POST['password'] == $_POST['repeatpassword']) {
         $password = md5($password);
-        $statement->execute();
-        header("location: login.php");
+        $sql = "INSERT INTO userdata (`password`, `email`, `username`, `firstname`, `lastname`)
+                VALUES ('$password', '$email', '$username', '$firstname', '$lastname')";
+        $pdo->exec($sql);
+        echo "New record created successfully";
+    } if ($_POST['password'] != $_POST['repeatpassword']){
+          echo "Bestätigen Sie Ihr Passwort.";
     }
-    else {
-        echo "Please confirm your password.";
     }
-}
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>"Name" | Registrierung</title>
+</head>
+<body>
+<h1>Registrieren Sie sich.</h1>
+<form action="register.php" method="post">
+    <label> <b>Username</b></label>
+    <input type="text" name="username" placeholder="Username" required>
+    <br>
+    <label> <b>Password</b></label>
+    <input type="password" name="password" placeholder="Password" required>
+    <br>
+    <label> <b>Please repeat your password</b></label>
+    <input type="password" name="repeatpassword" placeholder="Repeat your password" required>
+    <br>
+    <label> <b>E-Mail</b></label>
+    <input type="text" name="email" placeholder="E-mail" required>
+    <br>
+    <label> <b>First name</b></label>
+    <input type="text" name="firstname" placeholder="First name" required>
+    <br>
+    <label> <b>Last name</b></label>
+    <input type="text" name="lastname" placeholder="Last name" required>
+    <br>
+    <button type="submit" name="register" class="btn">Register</button>
+    <br>
+    <p>Already have an account? <a href="login.html">Sign in</a>.</p>
+</form>
+</body>
+</html>

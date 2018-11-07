11016d654f09904bd91dfc6ecf293b0178eb77a1
pigausername <rk067@hdm-stuttgart.de>
2018-11-07 11:55:49 +0100

    Renes update

---
+++ home1.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>
+        Mein Blog
+    </title>
+</head>
+<body>
+<h1>RAM</h1>
+<a href="logout.php">Logout</a><br>
+<a href="profil.php">Profil</a><br>
+
+<!-- Post schreiben -->
+
+<form action="do_post.php" method="post">
+    <textarea name="headline" placeholder="Titel" rows="2" cols="30"></textarea><br>
+    <input type="file" name="pic" accept="file_extension|audio/*|video/*|image/*|media_type"><br>
+    <textarea name="content" placeholder="Type your text here" rows="10" cols="30"></textarea>
+    <input type="submit">
+</form>
+
+<?php
+
+if(isset($_SESSION["angemeldet"]))
+{
+    echo"angemeldet.";
+
+}
+else
+{
+    echo"nicht angemeldet.";
+}
+
+include_once "userdata.php";
+
+
+// hole Content aus Datenbank
+
+echo"<br>";
+$content= $_POST["content"];
+echo $content;
+
+$pdo=new PDO($dsn, $dbuser, $dbpass);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+if($statement->execute()) {
+    while($row=$statement->fetch()) {
+        echo $row['id']." ".$row['content'];
+        echo "<br>";
+    }
+} else {
+    echo "Datenbank-Fehler:";
+    echo $statement->errorInfo()[2];
+    echo $statement->queryString;
+    die();
+}
+
+?>
+
+</body>
+
+</html>
+
+
+/**
+* Created by PhpStorm.
+* User: rene
+* Date: 07.11.2018
+* Time: 09:21
+*/

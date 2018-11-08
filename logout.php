ae9b4c0e82d580ae05327ab742f36dc0ab0590ff
pigausername <rk067@hdm-stuttgart.de>
2018-11-08 14:08:49 +0100

    Renes update

--- logout.php
+++ logout.php
@@ -2,10 +2,6 @@
 session_start();
 session_destroy();
 
-//Cookies entfernen
-setcookie("identifier","",time()-(3600*24*365));
-setcookie("securitytoken","",time()-(3600*24*365));
-
 echo" Logout erfolgreich.";
 header('Location: index.php');
 ?>

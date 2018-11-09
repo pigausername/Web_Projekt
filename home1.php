c5a098ee923ded893110175ff2b7a0def6cc2ccb
ab238 <ab238@hdm-stuttgart.de>
2018-11-09 12:16:55 +0100

    hallo

--- home1.php
+++ home1.php
@@ -8,11 +8,67 @@ include_once "header.php";
 <html lang="de">
 <head>
     <meta charset="utf-8">
-    <title>
-        RAM
-    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
+
+<style>
+
+    footer {
+        background-color: #555;
+        color: white;
+        padding: 15px;
+    }
+
+</style>
 <body>
+
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="#">Logo</a>
+
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+
+
+                <button class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-envelope"<></span>
+                </button>
+
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group input-group">
+                    <input type="text" class="form-control" placeholder="Search..">
+                    <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>
+                </div>
+            </form>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
 
 
 <!-- Post schreiben -->
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+/*
 include_once "header.php";
 
 if (isset($_POST['login'])) {
@@ -21,14 +21,33 @@ if (isset($_POST['login'])) {
         echo "blabla";
     }
 }
+*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <style>
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+    </style>
+
 </head>
+
 <body>
+
+
 Login:
 
 <form action="index.php" method="post">
@@ -47,11 +66,38 @@ Login:
 
 <a href="register.php">Register</a><br>
 
-</body>
-</html>
+
+
 <?php
+session_start();
+
+include_once "header.php";
+
+if (isset($_POST['login'])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $statement = $pdo->prepare("SELECT * FROM userdata WHERE email=:email AND password=:password");
+    if($statement->execute (array(':email' => $email, ':password' => $password))) {
+        if ($row = $statement->fetch()) {
+            //echo "angemeldet";
+            $_SESSION["angemeldet"] = $row["id"];
+            header("Location: home1.php");
+        } else {
+            echo "No authorization.";
+        }
+    }
+    else {
+        echo "blabla";
+    }
+}
+
 
 include_once "footer.php";
 ?>
 
+</body>
+</html>
+
+<?php
 

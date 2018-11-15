c5289c02296f91db5fc5dbdc1f1dbd2358b838a4
pigausername <rk067@hdm-stuttgart.de>
2018-11-15 18:49:59 +0100

    Renes update

--- follow.php
+++ follow.php
@@ -10,62 +10,28 @@ if ($profile_id!=$_SESSION["angemeldet"]) {
     // check ob wir dem Nutzer schon folgen
     $checkfollow=$pdo->prepare("SELECT followerid FROM followers WHERE userid=$profile_id");
     $checkfollow->execute();
-    $row=$checkfollow->fetch();
-
-
-
-
-
-    if ($checkfollow){
-        // Folgen schon gegenseitig --> unfollow
-
-        ?>
-        <form action="profile.php?userid=<?php echo $profile_id; ?>" method="post">
-            <input type="submit" name="unfollow" value="Unfollow">
-        </form>
-
-        <?php
-       // if (isset($_POST['unfollow'])){
-
-
-        }
-
-
-
-
-
-    else { //Folgen sich noch nicht --< Follow
+    $no=$checkfollow->rowCount();
+    if(!$no > 0){
         ?>
         <form action="profile.php?userid=<?php echo $profile_id; ?>" method="post">
             <input type="submit" name="follow" value="Follow">
         </form>
         <?php
         if (isset($_POST['follow'])) {
-            $follow = $pdo->prepare("INSERT INTO followers (`userid`, `followerid` VALUES '$profile_id', '$followerid' ");
+            $follow = $pdo->prepare("INSERT INTO followers (`userid`, `followerid`) VALUES ('$profile_id', '$followerid') ");
             if ($follow->execute()) {
-                echo "blablabla";
+                echo "blabla";
             }
         }
     }
+    else {
+        ?>
+        <!-- Folgen schon gegenseitig unfollow -->
+        <form action="profile.php?userid=<?php echo $profile_id; ?>" method="post">
+            <input type="submit" name="unfollow" value="Unfollow">
+        </form>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <?php
+        }
 }
 ?>

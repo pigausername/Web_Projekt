1d453670d1fcf177e6209e92b5b39fb098183c23
Minh_vh <40471018+mv065@users.noreply.github.com>
2018-11-23 10:26:55 +0100

    Unvollständiger Comment - Ergänzung

---
+++ comment.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div class="container">
+    <form method="POST" id="comment_form">
+        <div class="form-group">
+            <textarea name="comment" id="comment" class="form-control" placeholder="Enter Comment" rows="5" required></textarea>
+        </div>
+        <div class="form-group">
+            <input type="hidden" name="comment_id" id="comment_id" value="0" />
+            <input type="hidden" name="post_id" id="post_id" value=<?php
+            $post_id = $row["post_id"];
+            echo $post_id?>>
+            <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+        </div>
+    </form>
+    <span id="comment_message"></span>
+    <br />
+    <div id="display_comment"></div>
+</div>
+</body>
+</html>
+
+<script>
+
+    $(document).ready(function(){
+
+        $('#comment_form').on('submit', function(event){
+            event.preventDefault(); // Bedeutet in etwa Absicherung, was passieren würde, wenn der Code nicht funktioniert
+            var form_data = $(this).serialize();
+            $.ajax({
+                url:"add_comment.php",
+                method:"POST",
+                data:form_data,
+                dataType:"JSON",
+                success:function(data)
+                {
+                    if(data.error !== '')
+                    {
+                        $('#comment_form')[0].reset();
+                        $('#comment_message').html(data.error);
+                        $('#comment_id').val('0');
+                        load_comment();
+                    }
+                }
+            })
+        });
+
+        load_comment();
+
+        function load_comment()
+        {
+            $.ajax({
+                url:"fetch_comment.php?post_id=<?php echo $post_id?>",
+                method:"POST",
+                success:function(data)
+                {
+                    $('#display_comment').html(data);
+                }
+            })
+        }
+
+        $(document).on('click', '.reply', function(){
+            var comment_id = $(this).attr("id");
+            $('#comment_id').val(comment_id);
+            $('#comment').focus();
+
+        });
+
+    });
+</script>

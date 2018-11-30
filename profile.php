a2417c5937946f3abff39e3a2f53e9776c0b7a80
ab238 <ab238@hdm-stuttgart.de>
2018-11-30 08:49:06 +0100

    code optimiert.

--- profile.php
+++ profile.php
@@ -5,69 +5,70 @@ include_once "follow.php";
 
 $myprofile_id = $_SESSION["angemeldet"];
 $profile_id = $_GET['userid'];
-
-
 //Daten des jeweiligen Nutzers anzeigen
 $display_user = $pdo->prepare("SELECT * FROM userdata WHERE userid= $profile_id");
-    if($display_user->execute()) {
-        while ($row2 = $display_user->fetch()) {
-            ?>
+if($display_user->execute()) {
+    while ($row2 = $display_user->fetch()) {
+        ?>
 
-            <title><?php echo $row2['username'] ?></title>
-            <h3><?php $row2['username'] ?></h3>
-            <table>
-            <tr><td>Username:</td><td><?php echo $row2['username'] ?></td></tr>
+        <title><?php echo $row2['username'] ?></title>
+        <h1><?php echo $row2['username'] ?></h1>
+        <table>
             <tr><td><img src="pictures/<?php echo $row2['profilepic'] ?>"></td></tr>
+            <tr><td>Username:</td><td><?php echo $row2['username'] ?></td></tr>
             <tr><td>E-Mail:</td><td><?php echo $row2['email'] ?></td></tr>
             <tr><td>Firstname:</td><td><?php echo $row2['firstname'] ?></td></tr>
             <tr><td>Lastname:</td><td><?php echo $row2['lastname'] ?></td></tr>
-            </table>
-            <hr />
-            <?php
+        </table>
+        <hr />
+        <?php
 
-        }
-    } else {
-        echo "No user found";
     }
+} else {
+    echo "No user found";
+}
 
 
 //Posts des jeweiligen Nutzers anzeigen
 
-    $headline= $_POST ["headline"];
-    $file= $_POST ["file"];
-    $content= $_POST["content"];
+$headline= $_POST ["headline"];
+$file= $_POST ["file"];
+$content= $_POST["content"];
 
 
-    $sql = $pdo->prepare("SELECT * FROM posts WHERE userid= $profile_id ORDER BY date DESC");
-    if($sql->execute()) {
-        while ($row = $sql->fetch()) {
-            ?>
-            <form>
-                <table>
-                    <tr>
-                        <td><?php echo $row['headline'] ?></td>
-                    </tr>
-                    <tr>
-                        <td><img src="pictures/<?php echo $row['filename'] ?>"></td>
-                    <tr>
+$sql = $pdo->prepare("SELECT * FROM posts WHERE userid= $profile_id ORDER BY date DESC");
+if($sql->execute()) {
+    while ($row = $sql->fetch()) {
+        ?>
+        <form>
+            <table>
+                <tr>
+                    <td><?php echo $row['headline'] ?></td>
+                </tr>
+                <tr>
+                    <td><img src="pictures/<?php echo $row['filename'] ?>"></td>
+                <tr>
 
-                        <td><?php echo $row['content'] ?></td>
-                    </tr>
-                    <tr>
-                        <?php
-                        // Verweis auf Editseite
-                        if ($profile_id!=$_SESSION["angemeldet"]) {
+                    <td><?php echo $row['content'] ?></td>
+                </tr>
+                <tr>
+                    <?php
+                    // Verweis auf Editseite
+                    if ($profile_id==$_SESSION["angemeldet"]) {
 
-                            $post_id = $row['post_id'];
-                            echo '<td><a href="post_edit.php?post_id=' . $post_id . ' "> Edit </a></td>';
-                            }
-                        ?>
-                    </tr>
-                    <br>
-                </table>
-            </form>
-            <?php
-        }}
+                        $post_id = $row['post_id'];
+                        echo '<td><a href="post_edit.php?post_id=' . $post_id . ' "> Edit </a></td>';
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <td><a href="single_post.php?post_id=<?php echo $row["post_id"] ?>">Comment</a></td>
+                </tr>
+                <br>
+            </table>
+        </form>
+        <?php
+    }}
 include_once "footer.php";
 
 
--- search.php
+++ search.php
@@ -2,87 +2,117 @@
 include_once ("header.php");
 ?>
     <h1>Search by Username or Posts</h1>
-<form action="search.php" method="post">
-    <table>
-    <tr>
-        <td>Username:  </td>
-        <td><input type="text" name="search_username" placeholder=""></td>
-    </tr>
-    <tr>
-        <td>Posts: </td>
-        <td><input type="text" name="search_post" placeholder=""></td>
-    </tr>
-    <tr>
-        <td><button type="submit" name="search">Search</button></td>
-    </tr>
-    </table>
-</form>
+    <form action="search.php" method="post">
+        <table>
+            <tr>
+                <td>Username:  </td>
+                <td><input type="text" name="search_username" placeholder=""></td>
+            </tr>
+            <tr>
+                <td>Posts: </td>
+                <td><input type="text" name="search_post" placeholder=""></td>
+            </tr>
+            <tr>
+                <td><button type="submit" name="search">Search</button></td>
+            </tr>
+        </table>
+    </form>
 
 
 <?php
-    if (isset($_POST['search'])) {
+if (isset($_POST['search'])) {
 
-        echo "<h2>Your results:</h2>";
+    echo "<h2>Your results:</h2>";
 
-        $username = $_POST['search_username'];
+    $username = $_POST['search_username'];
 
-        $headline = $_POST['search_post'];
+    $headline = $_POST['search_post'];
 
-        $searchuser = $pdo->prepare("SELECT * FROM userdata WHERE username='$username'");
+    //Nutzer suchen
+
+    $searchuser = $pdo->prepare("SELECT * FROM userdata WHERE username='$username'");
 
 
-        if ($searchuser->execute()) {
-            while ($row = $searchuser->fetch()) {
-                ?>
+    if ($searchuser->execute()) {
+        while ($row = $searchuser->fetch()) {
+            $userid = $row['userid'];
+            ?>
 
-                <h3><?php echo $row['username'] ?></h3>
-
-                <?php
-                $userid = $row['userid'];
-
-                echo '<table>';
-                echo '<tr><td>Username:</td><td><a href="profile.php?userid='.$userid.'">' . $row['username'] . '</td></tr>';
-                //echo '<tr><td>Profile picture:</td><td><img src="'.$row["avatar"].'" width="100px" /></td></tr>';
-                echo '<tr><td>E-Mail:</td><td>' . $row['email'] . '</td></tr>';
-                echo '<tr><td>Firstname:</td><td>' . $row['firstname'] . '</td></tr>';
-                echo '<tr><td>Lastname:</td><td>' . $row['lastname'] . '</td></tr>';
-                echo '</table>';
-                echo '<hr />';
-            }
-        } else {
-            echo "No user found";
+            <h3><a href="profile.php?userid=<?php echo $row['userid'] ?>"><img src="pictures<?php echo $row['profilepic'] ?>"></a><?php echo $row['username'] ?></h3>
+            <table>
+                <tr>
+                    <td>Username:</td><td><a href="profile.php?userid=<?php echo $userid?>"><?php echo $row['username']?></a></td>
+                </tr>
+                <tr>
+                    <td>E-Mail:</td><td> <?php echo $row['email']?> </td>
+                </tr>
+                <tr>
+                    <td>First Name:</td><td> <?php echo $row['firstname']?> </td>
+                </tr>
+                <tr>
+                    <td>Last Name:</td><td> <?php echo $row['lastname']?> </td>
+                </tr>
+            </table>
+            <?php
         }
-
-        $searchpost = $pdo->prepare("SELECT * FROM posts WHERE headline='$headline'");
-
-
-
-        //$searcheditor = $pdo->prepare("SELECT username FROM userdata WHERE userid=");
-
-
-
-        if ($searchpost->execute() //AND $searcheditor->execute()
-        ) {
-            while ($row = $searchpost->fetch() //AND ($row2 = $searcheditor->fetch())
-                ) {
-                ?>
-
-                <h3><?php echo"'".$row['headline']."'"." by ". $row['userid'] ?></h3>
-
-                <?php
-                echo '<table>';
-                echo '<tr><td>Headline:</td><td>' . $row['headline'] . '</td></tr>';
-                //echo '<tr><td>File:</td><td><img src="'.$row["avatar"].'" width="100px" /></td></tr>';
-                //echo '<tr><td>By:</td><td>' . $row2['username'] . '</td></tr>';
-                echo '<tr><td>Content:</td><td>' . $row['content'] . '</td></tr>';
-                echo '<tr><td>Date:</td><td>' . $row['date'] . '</td></tr>';
-                echo '</table>';
-                echo '<hr />';
-            }
-        } else {
-            echo "No post found";
-        }
+    } else {
+        echo "No user found";
     }
+
+    //Post suchen
+
+    $searchpost = $pdo->prepare("SELECT * FROM posts WHERE headline='$headline'");
+
+    if ($searchpost->execute()) {
+
+        while ($row2 = $searchpost->fetch()) {
+            $editor_id = $row2['userid'];
+
+            //Informationen des Verfassers
+
+            $display_editor = $pdo->prepare("SELECT * FROM userdata WHERE userid= $editor_id");
+            $display_editor->execute();
+
+            $row3 = $display_editor->fetch();
+            ?>
+            <h3>'<?php echo $row2['headline']?>' by <a href="profile.php?userid=<?php echo $row2['userid']?>"><?php echo $row3['username'] ?></a></h3>
+
+            <a href="profile.php?userid=<?php echo $row3['userid'] ?>"><img src="pictures<?php echo $row3['profilepic'] ?>"></a>
+            <table>
+                <tr>
+                    <td>
+
+                        <?php
+                        // Hierbei wird überprüft, ob der jeweilige Post ein Bild beinhält --> Unovllständig
+
+                        $checkpic=$pdo->prepare("SELECT filename FROM posts WHERE userid= $feedid OR userid= $myid");
+                        $checkpic->execute();
+
+                        $no=$checkpic->rowCount();
+                        if($no > 0){
+                        ?>
+                        <img src="pictures <?php echo $row2['filename'] ?>"></td>
+                    <?php
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <td>Content:</td><td><?php echo $row2['content']?></td>
+                </tr>
+                <tr>
+                    <td>Date:</td><td><?php echo $row2['date']?></td>
+                </tr>
+                <tr>
+                    <td><a href="single_post.php?post_id=<?php echo $row2["post_id"] ?>">Comment</a></td>
+                </tr>
+                <hr />
+            </table>
+            <?php
+        }
+    } else {
+        echo "No post found";
+    }
+}
 include_once("footer.php");
 
 ?>

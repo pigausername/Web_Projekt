50ff1a5debd8758c49eebc4262f45fce468373e3
pigausername <rk067@hdm-stuttgart.de>
2019-01-17 11:47:28 +0100

    notification update

--- single_post.php
+++ single_post.php
@@ -3,14 +3,22 @@ session_start();
 include_once "header.php";
 
 
+
+$userid = $_SESSION["angemeldet"];
 $post_id = $_GET["post_id"];
 
+// Delete Notifications if existing
+$check_notify_stat = $pdo->prepare("DELETE FROM notification WHERE post_id='$post_id' AND receiverid='$userid'");
+$check_notify_stat->execute();
+
+// Get Posts from Database and fetch to row
 $display_post = $pdo->prepare("SELECT * FROM posts WHERE post_id='$post_id'");
 $display_post->execute();
 $row = $display_post->fetch();
 
 $editor_id = $row["userid"];
 
+// Get Userdata from Database
 $display_editor = $pdo->prepare("SELECT * FROM userdata WHERE userid='$editor_id'");
 $display_editor->execute();
 $row2 = $display_editor->fetch();
